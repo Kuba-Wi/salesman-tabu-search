4b24a3c37a130b543fa17d788966bfbbde0d8221
kuba <winczukjakub@gmail.com>
2023-11-13 17:09:13 +0100

    small change in main

--- main.cpp
+++ main.cpp
@@ -7,20 +7,18 @@ int main() {
     PathFinder pathFinder;
     pathFinder.readDistancesFromFile("../distances2.txt");
     auto bestPath = pathFinder.findBestPath();
-    auto length = pathFinder.getPathLength(bestPath);
     for (auto node : bestPath) {
         std::cout << node << " ";
     }
     std::cout << "\n";
-    std::cout << length << "\n";
+    std::cout << pathFinder.getPathLength(bestPath) << "\n";
 
     GeneticFinder geneticFinder;
     geneticFinder.readDistancesFromFile("../distances2.txt");
     bestPath = geneticFinder.findBestPath();
-    length = geneticFinder.getPathLength(bestPath);
     for (auto node : bestPath) {
         std::cout << node << " ";
     }
     std::cout << "\n";
-    std::cout << length << "\n";
+    std::cout << geneticFinder.getPathLength(bestPath) << "\n";
 }
